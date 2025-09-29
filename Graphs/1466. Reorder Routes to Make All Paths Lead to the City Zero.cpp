f3617c3b910857f09b4912402c35955a116b739c
Paras Chand Ramola <parasramola5@gmail.com>
2025-09-29 12:26:06 +0530

    Create 1466. Reorder Routes to Make All Paths Lead to the City Zero.cpp

---
+++ Graphs/1466. Reorder Routes to Make All Paths Lead to the City Zero.cpp
@@ -0,0 +1,47 @@
+//Appraoch 1:BFS
+// T.C:0(N+M)
+//Each node is visited once → O(n) 
+// For each node, we iterate over all neighbors → total iterations over adjacency lists = O(2m) 
+
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        // Build adjacency list
+        // Each pair: (neighbor, isOriginalDirection)
+        // isOriginalDirection = 1 -> real road(given)
+        // isOriginalDirection = 0 -> fake road
+        unordered_map<int, list<pair<int, int>>> adjList;
+        for (auto& road : connections) {
+            int u = road[0];
+            int v = road[1];
+
+            adjList[u].push_back({v, 1}); // original road u->v
+            adjList[v].push_back({u, 0}); // fake/virtual road v->u
+        }
+
+        vector<bool> visited(n, false); // track visited cities
+        int reorderCnt = 0;             // number of roads to reverse
+
+        queue<int> q;
+        q.push(0); // start BFS from city 0
+
+        while (!q.empty()) {
+            int currCity = q.front();
+            q.pop();
+            visited[currCity] = true;
+
+            // Explore all neighbors
+            for (auto& [neigh, needsReorder] : adjList[currCity]) {
+                if (!visited[neigh]) {
+                    if (needsReorder ==
+                        1) { // road goes away from 0, needs reversal
+                        reorderCnt++;
+                    }
+                    q.push(neigh);
+                }
+            }
+        }
+
+        return reorderCnt;
+    }
+};
